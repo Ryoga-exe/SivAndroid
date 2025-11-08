4455a363134770e6b3d2c6e1dafd4f5874bb2eac
Ryoga-exe <git@ryoga.dev>
2025-11-08 22:51:30 +0900

    wip

--- Android/app/src/main/cpp/Main.cpp
+++ Android/app/src/main/cpp/Main.cpp
@@ -1,9 +1,4 @@
-#include <sstream>
-
-extern std::ostream aout;
-
 #include <Siv3D.hpp>
 
 void Main() {
-    aout << "Hello world" << std::endl;
 }
--- Siv3D/src/Siv3D-Platform/Android/Siv3D/Siv3DMain.cpp
+++ Siv3D/src/Siv3D-Platform/Android/Siv3D/Siv3DMain.cpp
@@ -12,27 +12,25 @@ void Main();
 
 class AndroidOut : public std::stringbuf {
 public:
-  /*!
-   * Creates a new output stream for logcat
-   * @param kLogTag the log tag to output
-   */
-  inline AndroidOut(const char *kLogTag) : logTag_(kLogTag) {}
+    /*!
+     * Creates a new output stream for logcat
+     * @param kLogTag the log tag to output
+     */
+    inline AndroidOut(const char *kLogTag) : logTag_(kLogTag) {}
 
 protected:
-  virtual int sync() override {
-    __android_log_print(ANDROID_LOG_DEBUG, logTag_, "%s", str().c_str());
-    str("");
-    return 0;
-  }
+    virtual int sync() override {
+        __android_log_print(ANDROID_LOG_DEBUG, logTag_, "%s", str().c_str());
+        str("");
+        return 0;
+    }
 
 private:
-  const char *logTag_;
+    const char *logTag_;
 };
 
 AndroidOut androidOut("AO");
 std::ostream aout(&androidOut);
-
-extern "C" {
 
 /*!
  * Handles commands sent to this Android application
@@ -40,30 +38,30 @@ extern "C" {
  * @param cmd the command to handle
  */
 void handle_cmd(android_app *pApp, int32_t cmd) {
-  switch (cmd) {
-  case APP_CMD_INIT_WINDOW:
-    // A new window is created, associate a renderer with it. You may replace
-    // this with a "game" class if that suits your needs. Remember to change all
-    // instances of userData if you change the class here as a reinterpret_cast
-    // is dangerous this in the android_main function and the
-    // APP_CMD_TERM_WINDOW handler case. pApp->userData = new Renderer(pApp);
-    break;
-  case APP_CMD_TERM_WINDOW:
-    // The window is being destroyed. Use this to clean up your userData to
-    // avoid leaking resources.
-    //
-    // We have to check if userData is assigned just in case this comes in
-    // really quickly
-    //            if (pApp->userData) {
-    //                //
-    //                auto *pRenderer = reinterpret_cast<Renderer
-    //                *>(pApp->userData); pApp->userData = nullptr; delete
-    //                pRenderer;
-    //            }
-    break;
-  default:
-    break;
-  }
+    switch (cmd) {
+        case APP_CMD_INIT_WINDOW:
+            // A new window is created, associate a renderer with it. You may replace
+            // this with a "game" class if that suits your needs. Remember to change all
+            // instances of userData if you change the class here as a reinterpret_cast
+            // is dangerous this in the android_main function and the
+            // APP_CMD_TERM_WINDOW handler case. pApp->userData = new Renderer(pApp);
+            break;
+        case APP_CMD_TERM_WINDOW:
+            // The window is being destroyed. Use this to clean up your userData to
+            // avoid leaking resources.
+            //
+            // We have to check if userData is assigned just in case this comes in
+            // really quickly
+            //            if (pApp->userData) {
+            //                //
+            //                auto *pRenderer = reinterpret_cast<Renderer
+            //                *>(pApp->userData); pApp->userData = nullptr; delete
+            //                pRenderer;
+            //            }
+            break;
+        default:
+            break;
+    }
 }
 
 /*!
@@ -76,73 +74,57 @@ void handle_cmd(android_app *pApp, int32_t cmd) {
  *         false for all other input devices.
  */
 bool motion_event_filter_func(const GameActivityMotionEvent *motionEvent) {
-  auto sourceClass = motionEvent->source & AINPUT_SOURCE_CLASS_MASK;
-  return (sourceClass == AINPUT_SOURCE_CLASS_POINTER ||
-          sourceClass == AINPUT_SOURCE_CLASS_JOYSTICK);
+    auto sourceClass = motionEvent->source & AINPUT_SOURCE_CLASS_MASK;
+    return (sourceClass == AINPUT_SOURCE_CLASS_POINTER ||
+            sourceClass == AINPUT_SOURCE_CLASS_JOYSTICK);
 }
 
 /*!
  * This the main entry point for a native activity
  */
+__attribute__((used))
 void android_main(struct android_app *pApp) {
-  // Can be removed, useful to ensure your code is running
-  aout << "Welcome to android_main" << std::endl;
+    aout << "Welcome to android_main" << std::endl;
 
-  // Register an event handler for Android events
-  pApp->onAppCmd = handle_cmd;
+    pApp->onAppCmd = handle_cmd;
 
-  // Set input event filters (set it to NULL if the app wants to process all
-  // inputs). Note that for key inputs, this example uses the default
-  // default_key_filter() implemented in android_native_app_glue.c.
-  android_app_set_motion_event_filter(pApp, motion_event_filter_func);
+    android_app_set_motion_event_filter(pApp, motion_event_filter_func);
 
-  // This sets up a typical game/event loop. It will run until the app is
-  // destroyed.
-  Main();
-  do {
-    // Process all pending events before running game logic.
-    bool done = false;
-    while (!done) {
-      // 0 is non-blocking.
-      int timeout = 0;
-      int events;
-      android_poll_source *pSource;
-      int result = ALooper_pollOnce(timeout, nullptr, &events,
-                                    reinterpret_cast<void **>(&pSource));
-      switch (result) {
-      case ALOOPER_POLL_TIMEOUT:
-        [[clang::fallthrough]];
-      case ALOOPER_POLL_WAKE:
-        // No events occurred before the timeout or explicit wake. Stop checking
-        // for events.
-        done = true;
-        break;
-      case ALOOPER_EVENT_ERROR:
-        aout << "ALooper_pollOnce returned an error" << std::endl;
-        break;
-      case ALOOPER_POLL_CALLBACK:
-        break;
-      default:
-        if (pSource) {
-          pSource->process(pApp, pSource);
+    // LOG_TRACE(U"Main() ---");
+
+    Main();
+
+    // LOG_TRACE(U"--- Main()");
+
+    do {
+        // Process all pending events before running game logic.
+        bool done = false;
+        while (!done) {
+            // 0 is non-blocking.
+            int timeout = 0;
+            int events;
+            android_poll_source *pSource;
+            int result = ALooper_pollOnce(timeout, nullptr, &events,
+                                          reinterpret_cast<void **>(&pSource));
+            switch (result) {
+                case ALOOPER_POLL_TIMEOUT:
+                    [[clang::fallthrough]];
+                case ALOOPER_POLL_WAKE:
+                    // No events occurred before the timeout or explicit wake. Stop checking
+                    // for events.
+                    done = true;
+                    break;
+                case ALOOPER_EVENT_ERROR:
+                    aout << "ALooper_pollOnce returned an error" << std::endl;
+                    break;
+                case ALOOPER_POLL_CALLBACK:
+                    break;
+                default:
+                    if (pSource) {
+                        pSource->process(pApp, pSource);
+                    }
+            }
         }
-      }
-    }
 
-    // Check if any user data is associated. This is assigned in handle_cmd
-    //        if (pApp->userData) {
-    //            // We know that our user data is a Renderer, so reinterpret
-    //            cast it. If you change your
-    //            // user data remember to change it here
-    //            auto *pRenderer = reinterpret_cast<Renderer
-    //            *>(pApp->userData);
-    //
-    //            // Process game input
-    //            pRenderer->handleInput();
-    //
-    //            // Render a frame
-    //            pRenderer->render();
-    //        }
-  } while (!pApp->destroyRequested);
-}
+    } while (!pApp->destroyRequested);
 }
